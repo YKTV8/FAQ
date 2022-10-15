736dd915ef573fdf536b4946ad6796eb3b04ead5
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-10-14 23:34:04 -0700

    Completed code
    javascript written on pause.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const toggle = document.querySelectorAll('button')
+const toggle = document.querySelectorAll('.faq')
+console.log(toggle)
+
+toggle.forEach((toggle)=>{
+    toggle.addEventListener('click',()=>{
+    removeActive()
+    toggle.classList.add('active')
+
+})
+})
+function removeActive(){
+    toggle.forEach(toggle=>{
+        toggle.classList.remove('active')
+    })
+}
