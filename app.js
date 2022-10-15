8efcbf3d1c8ebe9fb45b408df75a5b8b21afa735
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-10-14 23:44:05 -0700

    Completed code
    as instructed

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const toggle = document.querySelectorAll('.faq')
-console.log(toggle)
+// const toggle = document.querySelectorAll('.faq')
+// //console.log(toggle)
 
-toggle.forEach((toggle)=>{
-    toggle.addEventListener('click',()=>{
-    removeActive()
-    toggle.classList.add('active')
+// toggle.forEach((toggle)=>{
+//     toggle.addEventListener('click',()=>{
+//     removeActive()
+//     toggle.classList.add('active')
 
+// })
+// })
+// function removeActive(){
+//     toggle.forEach(toggle=>{
+//         toggle.classList.remove('active')
+//     })
+// }
+
+const toggleThru = document.querySelectorAll('.faq-toggle')
+
+toggleThru.forEach(toggle =>{
+    toggle.addEventListener('click', () =>{
+        toggle.parentNode.classList.toggle('active')
+        })
 })
-})
-function removeActive(){
-    toggle.forEach(toggle=>{
-        toggle.classList.remove('active')
-    })
-}
